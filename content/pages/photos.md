f0c1d0dc9a54d1990e9b9f65ea601e8d09231e38
Tim Novis <tim.novis@me.com>
2019-08-03 16:46:31 +0100

    Timstagram!

---
+++ content/pages/photos.md
@@ -0,0 +1,7 @@
+---
+title: 'Photos'
+date: 2019-08-03T15:58:00+01:00
+draft: false
+type: 'pages'
+layout: 'photo-stream'
+---
---
+++ functions/src/timstagram.js
@@ -0,0 +1,91 @@
+const Octokit = require('@octokit/rest');
+
+const github = Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const owner = 'timnovis';
+const repo = 'novis';
+
+const postTemplate = (caption, date, id) => `---
+title: '${caption}'
+date: ${date}
+draft: false
+tags: 'photo'
+---
+
+![${caption}](/media/timstagram/${id}.jpg "${caption}")`;
+
+exports.handler = async function(event, context) {
+  const { httpMethod: method, headers, body, path } = event;
+
+  if (method === 'GET') {
+    return { statusCode: 501, body: 'Nothing to see here' };
+  }
+
+  try {
+    const { image, caption, filename } = JSON.parse(body);
+
+    const id = `${filename}-${Date.now()}`;
+
+    const [blob, branch] = await Promise.all([
+      github.git.createBlob({
+        owner,
+        repo,
+        content: image,
+        encoding: 'base64',
+      }),
+      github.gitdata.getRef({
+        owner,
+        repo,
+        ref: 'heads/master',
+      }),
+    ]);
+
+    const tree = await github.git.createTree({
+      owner,
+      repo,
+      tree: [
+        {
+          path: `static/media/timstagram/${id}.jpg`,
+          mode: '100644',
+          type: 'blob',
+          sha: blob.data.sha,
+        },
+        {
+          path: `content/posts/image-${id}.md`,
+          mode: '100644',
+          type: 'blob',
+          content: postTemplate(caption, new Date().toISOString().slice(0, -14), id),
+        },
+      ],
+      base_tree: branch.data.object.sha,
+    });
+
+    const commit = await github.git.createCommit({
+      owner,
+      repo,
+      message: `New photo from Tim's iPhone`,
+      tree: tree.data.sha,
+      parents: [branch.data.object.sha],
+    });
+
+    await github.git.updateRef({
+      owner,
+      repo,
+      ref: 'heads/master',
+      sha: commit.data.sha,
+      force: true,
+    });
+
+    return {
+      statusCode: 200,
+      body: 'success',
+    };
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: e.toString(),
+    };
+  }
+};
